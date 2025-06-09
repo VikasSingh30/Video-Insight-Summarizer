231b1d1024579030bbf5ec41080951998c50a175
Vikas <singhvikas.sv30@gmail.com>
2025-06-09 17:09:30 +0000

    authroutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './db/mongo.js';
+import authRoutes from '../src/routes/authRoutes.js';
+
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 
 app.get('/health', (req, res) => {
   res.status(200).send('Server is running');
